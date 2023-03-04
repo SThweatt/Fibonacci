6294f654f67875b612a8d25fdfb00c6c0a2852c3
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-03-04 08:01:59 -0800

    Add project files.

---
+++ Fibonacci.py
@@ -0,0 +1,21 @@
+# Sara Thweatt
+# CIS261
+# Fibonacci
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+print('The Fibonacci series for 16')
+
+def main():
+    for i in range(16):
+        print(fib(i), end=', ')
+    print('...')
+
+if __name__ == '__main__':
+    main()
